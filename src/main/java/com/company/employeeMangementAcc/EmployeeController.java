af1d43e8bb31f94cfa6f0e5028924ec4bc4b938b
harsh538 <harshadvele538@gmail.com>
2024-02-29 22:20:34 +0530

    Updated logs added Task3 in Task2 branch

--- src/main/java/com/company/employeeMangementAcc/EmployeeController.java
+++ src/main/java/com/company/employeeMangementAcc/EmployeeController.java
@@ -33,6 +33,7 @@ public class EmployeeController {
         logger.error("This is an ERROR message");
         logger.fatal("This is a FATAL message");
         System.out.println("Hello harshad Task1");
+        System.out.println("Task3");
     }
 
     @PostMapping(value="/save",consumes= MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
